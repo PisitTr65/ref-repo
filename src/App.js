598f89e07354dff99b2c024665c847e265ff36ea
PisitTr65 <pisit.tr.65@ubu.ac.th>
2023-07-18 12:13:11 +0700

    ref-repo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Table } from "react-bootstrap";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import Contact from "./Contact";
+import { useRef } from "react";
 
 export function Layout() {
   return (
@@ -19,7 +20,7 @@ export function Layout() {
         Main
       </NavLink>
       <NavLink
-        to="/Product"
+        to="/product"
         className="link px-2"
         style={({ isActive }) => {
           return {
@@ -45,20 +46,69 @@ export function Layout() {
 }
 
 function Index() {
+  const header1 = useRef();
   return (
     <>
       <Layout />
-      <h3>Hello</h3>
-      <Button>Click Me</Button>
+      <h3 ref={header1}>Hello</h3>
+      <Button
+        onClick={() => {
+          header1.current.innerHTML = "React";
+        }}
+      >
+        Click Me
+      </Button>
     </>
   );
 }
 
 function Product() {
+  const table = useRef();
+  const tr = useRef([]);
+  const DeleteRow = (i) => {
+    const Index = tr.current[i].rowIndex;
+    table.current.deleteRow(Index);
+  };
+
+  const date = [
+    ["กางเกงยืนส์", 1200],
+    ["เสื้อยืด", 350],
+    ["กางเกงขาสั้น", 450],
+    ["หมวก", 500],
+  ];
+
   return (
     <>
       <Layout />
       <h4>products</h4>
+      <Table striped bordered hover className="my-3" ref={table}></Table>
+      <thead>
+        <tr>
+          <th>Product Name</th>
+          <th>Price</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {date.map((item, i) => {
+          return (
+            <tr
+              ref={(el) => {
+                tr.current[i] = el;
+              }}
+              key={i}
+            >
+              <td>{item[0]}</td>
+              <td>{item[1]}</td>
+              <td className="text-center">
+                <Button variant="danger" onClick={() => DeleteRow(1)}>
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
     </>
   );
 }
@@ -70,7 +120,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/main" element={<Index />} />
-          <Route path="/products" element={<Product />} />
+          <Route path="/product" element={<Product />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Container>
